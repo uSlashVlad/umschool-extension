de23d8d9980ccb16bea35f11fe946df5bf87fcdf
Vladislav Nagorny <u.slash.vlad@gmail.com>
2020-11-21 21:10:33 +0300

    Added chrome capability

--- injected.js
+++ injected.js
@@ -19,97 +19,95 @@ console.log('start');
 
 // Получение массива аргументов ссылки
 const urls = document.URL.split('/'),
-  darkThemeUrl = browser.extension.getURL('dark.css');
+  darkThemeUrl = chrome.extension.getURL('dark.css');
 urls.splice(0, 3);
 urls.splice(urls.length - 1, 1);
 
 let styles = '',
-  ytUrl,
-  data;
+  ytUrl;
 
-init();
+chrome.storage.local.get(main);
 
-async function init() {
-  data = await browser.storage.local.get();
+function main(data) {
   console.log(data);
   if (data[settingsKeys[3]]) {
     document.head.innerHTML += `<link id="dark-theme-link" rel="stylesheet" href="${darkThemeUrl}" />`;
   }
+
+  document.onreadystatechange = async () => {
+    const state = document.readyState;
+    console.log(state);
+    if (state === 'interactive') {
+      if (urls[0] === 'lessons' || urls[1] === 'lessons') {
+        ytUrl = createYTUrl();
+        if (data['umsc_settings_autoloading']) {
+          window.open(ytUrl);
+        } else {
+          let elem = document.body.getElementsByClassName('preview-title')[0];
+          if (!elem)
+            elem = document.body.getElementsByClassName('date-container')[0];
+
+          if (elem) {
+            styles += button_main_style;
+            elem.innerHTML += buttonsForYT;
+
+            const button1 = document.body.getElementsByClassName(
+              'yt_button_script'
+            )[0];
+            const buttonnew = document.body.getElementsByClassName(
+              'yt_button_script'
+            )[1];
+
+            if (button1) {
+              button1.onclick = ytStart;
+            }
+            if (buttonnew) {
+              buttonnew.onclick = ytStartNew;
+            }
+          }
+        }
+      } else if (
+        (urls[1] === 'lessons' && urls[3] === 'homework') ||
+        (urls[0] === 'homework' && urls[1] === 'submissions')
+      ) {
+        if (data[settingsKeys[1]]) {
+          styles += hmMyStyle;
+        }
+        if (data[settingsKeys[2]]) {
+          const fieldsToolbarElems = document.body.getElementsByClassName(
+            'fr-toolbar'
+          );
+          for (let i = 0; i < fieldsToolbarElems.length; i++) {
+            fieldsToolbarElems[i].remove();
+            i--;
+          }
+        }
+      }
+
+      if (styles) document.head.innerHTML += '<style> ' + styles + ' </style>';
+    }
+
+    try {
+      // Убирание бесполезной картинки слева
+      if (experiment) {
+        var uselessImageElement = document.body.getElementsByClassName(
+          'if-mobile'
+        )[1];
+        uselessImageElement.innerHTML = '';
+
+        styles += scroll_custom_style;
+      }
+
+      // Замена медведя
+      var bearImageElem = document.body.getElementsByClassName(
+        'bear-notifier-img'
+      )[0];
+      if (bearImageElem) {
+        bearImageElem.src = bearImageNewSrc;
+      }
+    } catch (error) {}
+  };
 }
-
-document.onreadystatechange = async () => {
-  const state = document.readyState;
-  console.log(state);
-  if (state === 'interactive') {
-    if (urls[0] === 'lessons' || urls[1] === 'lessons') {
-      ytUrl = createYTUrl();
-      if (data['umsc_settings_autoloading']) {
-        window.open(ytUrl);
-      } else {
-        let elem = document.body.getElementsByClassName('preview-title')[0];
-        if (!elem)
-          elem = document.body.getElementsByClassName('date-container')[0];
-
-        if (elem) {
-          styles += button_main_style;
-          elem.innerHTML += buttonsForYT;
-
-          const button1 = document.body.getElementsByClassName(
-            'yt_button_script'
-          )[0];
-          const buttonnew = document.body.getElementsByClassName(
-            'yt_button_script'
-          )[1];
-
-          if (button1) {
-            button1.onclick = ytStart;
-          }
-          if (buttonnew) {
-            buttonnew.onclick = ytStartNew;
-          }
-        }
-      }
-    } else if (
-      (urls[1] === 'lessons' && urls[3] === 'homework') ||
-      (urls[0] === 'homework' && urls[1] === 'submissions')
-    ) {
-      if (data[settingsKeys[1]]) {
-        styles += hmMyStyle;
-      }
-      if (data[settingsKeys[2]]) {
-        const fieldsToolbarElems = document.body.getElementsByClassName(
-          'fr-toolbar'
-        );
-        for (let i = 0; i < fieldsToolbarElems.length; i++) {
-          fieldsToolbarElems[i].remove();
-          i--;
-        }
-      }
-    }
-
-    if (styles) document.head.innerHTML += '<style> ' + styles + ' </style>';
-  }
-
-  try {
-    // Убирание бесполезной картинки слева
-    if (experiment) {
-      var uselessImageElement = document.body.getElementsByClassName(
-        'if-mobile'
-      )[1];
-      uselessImageElement.innerHTML = '';
-
-      styles += scroll_custom_style;
-    }
-
-    // Замена медведя
-    var bearImageElem = document.body.getElementsByClassName(
-      'bear-notifier-img'
-    )[0];
-    if (bearImageElem) {
-      bearImageElem.src = bearImageNewSrc;
-    }
-  } catch (error) {}
-};
 
 function ytStart() {
   window.location = ytUrl;
--- popup/popup.js
+++ popup/popup.js
@@ -7,61 +7,61 @@ const settingsKeys = [
 
 document.addEventListener('DOMContentLoaded', async function () {
   // Загрузка данных из хранилища
-  const data = await browser.storage.local.get();
+  chrome.storage.local.get((data) => {
+    data[settingsKeys[0]] = data[settingsKeys[0]] === true ? true : false;
+    data[settingsKeys[1]] = data[settingsKeys[1]] === true ? true : false;
+    data[settingsKeys[2]] = data[settingsKeys[2]] === true ? true : false;
+    data[settingsKeys[3]] = data[settingsKeys[3]] === true ? true : false;
 
-  data[settingsKeys[0]] = data[settingsKeys[0]] === true ? true : false;
-  data[settingsKeys[1]] = data[settingsKeys[1]] === true ? true : false;
-  data[settingsKeys[2]] = data[settingsKeys[2]] === true ? true : false;
-  data[settingsKeys[3]] = data[settingsKeys[3]] === true ? true : false;
+    // console.log(data[settings_keys[3]]);
 
-  // console.log(data[settings_keys[3]]);
+    console.log(data);
 
-  console.log(data);
+    // Получение блоков с переключателями
+    const a = document.getElementById('setting_autoloading');
+    const b = document.getElementById('setting_remtoolbars');
+    const c = document.getElementById('setting_experimental');
+    const d = document.getElementById('setting_darktheme');
 
-  // Получение блоков с переключателями
-  const a = document.getElementById('setting_autoloading');
-  const b = document.getElementById('setting_remtoolbars');
-  const c = document.getElementById('setting_experimental');
-  const d = document.getElementById('setting_darktheme');
+    // Получение самих переключателей
+    const a_check = document.getElementById('setting_autoloading_check');
+    const b_check = document.getElementById('setting_remtoolbars_check');
+    const c_check = document.getElementById('setting_experimental_check');
+    const d_check = document.getElementById('setting_darktheme_check');
 
-  // Получение самих переключателей
-  const a_check = document.getElementById('setting_autoloading_check');
-  const b_check = document.getElementById('setting_remtoolbars_check');
-  const c_check = document.getElementById('setting_experimental_check');
-  const d_check = document.getElementById('setting_darktheme_check');
-
-  // Установка значений из хранилища на переключатели
-  a_check.checked = data[settingsKeys[0]];
-  b_check.checked = data[settingsKeys[2]];
-  c_check.checked = data[settingsKeys[1]];
-  d_check.checked = data[settingsKeys[3]];
-
-  a.addEventListener('click', async () => {
-    data[settingsKeys[0]] = !data[settingsKeys[0]];
+    // Установка значений из хранилища на переключатели
     a_check.checked = data[settingsKeys[0]];
-    await updateStorage(data);
-  });
-
-  b.addEventListener('click', async () => {
-    data[settingsKeys[2]] = !data[settingsKeys[2]];
     b_check.checked = data[settingsKeys[2]];
-    await updateStorage(data);
-  });
-
-  c.addEventListener('click', async () => {
-    data[settingsKeys[1]] = !data[settingsKeys[1]];
     c_check.checked = data[settingsKeys[1]];
-    await updateStorage(data);
-  });
-
-  d.addEventListener('click', async () => {
-    data[settingsKeys[3]] = !data[settingsKeys[3]];
     d_check.checked = data[settingsKeys[3]];
-    await updateStorage(data);
+
+    a.addEventListener('click', async () => {
+      data[settingsKeys[0]] = !data[settingsKeys[0]];
+      a_check.checked = data[settingsKeys[0]];
+      await updateStorage(data);
+    });
+
+    b.addEventListener('click', async () => {
+      data[settingsKeys[2]] = !data[settingsKeys[2]];
+      b_check.checked = data[settingsKeys[2]];
+      await updateStorage(data);
+    });
+
+    c.addEventListener('click', async () => {
+      data[settingsKeys[1]] = !data[settingsKeys[1]];
+      c_check.checked = data[settingsKeys[1]];
+      await updateStorage(data);
+    });
+
+    d.addEventListener('click', async () => {
+      data[settingsKeys[3]] = !data[settingsKeys[3]];
+      d_check.checked = data[settingsKeys[3]];
+      await updateStorage(data);
+    });
   });
 });
 
 async function updateStorage(data) {
   console.log(data);
-  await browser.storage.local.set(data);
+  chrome.storage.local.set(data);
 }
